057f48dfa84b97b438c97b2cde78825b0cceb859
Lena <osnapitzlena@gmail.com>
2024-01-04 22:23:56 +0300

    task_2.3

---
+++ Task 2.3/task_2.3.js
@@ -0,0 +1,8 @@
+const   titleProject = 'Оценка стоимости';
+        screensValue = 'шаблонные, с уникальным дизайном, с анимациями';
+        screenPrice = 100 + '$';
+        percentage = 10;
+        fullPrice = 1500;
+
+        console.log(titleProject, screensValue);
+
